b03d588187de0db18dd1095c6f3a11faa2f38adc
Arthur <merklingerarthur@gmail.com>
2024-04-23 18:35:48 +0200

    fragment_parcel_detail.xml to fragment_parcel_detailedview.xml
    also changed it to the correct class "layout"

--- app/src/main/java/com/km/parcelorganizer/ui/parcels/ParcelsDetailView.kt
+++ app/src/main/java/com/km/parcelorganizer/ui/parcels/ParcelsDetailView.kt
@@ -4,11 +4,11 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
-import android.widget.TextView
 import com.km.parcelorganizer.R
 import com.km.parcelorganizer.model.Parcel
 
-class ParcelDetailFragment : Fragment() {
+class ParcelsDetailView : Fragment() {
+
     private lateinit var parcel: Parcel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,27 +24,27 @@ class ParcelDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_parcel_detail, container, false)
+        return inflater.inflate(R.layout.fragment_parcel_detailedview, container, false)
     }
 
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Bind the parcel data to the views
-        view.findViewById<TextView>(R.id.tvParcelId).text = "ID: ${parcel.id}"
-        view.findViewById<TextView>(R.id.tvParcelName).text = "Name: ${parcel.title}"
-        view.findViewById<TextView>(R.id.tvParcelDescription).text = "Description: ${parcel.additionalInformation}"
-        view.findViewById<TextView>(R.id.tvParcelStatus).text = "Status: ${parcel.parcelStatus}"
-        view.findViewById<TextView>(R.id.tvParcelSender).text = "Sender: ${parcel.sender}"
-        view.findViewById<TextView>(R.id.tvParcelCourier).text = "Courier: ${parcel.courier}"
-        view.findViewById<TextView>(R.id.tvParcelTrackingUrl).text = "Last Updated: ${parcel.trackingUrl}"
+//        // Bind the parcel data to the views
+//        view.findViewById<TextView>(R.id.tvParcelId).text = "ID: ${parcel.id}"
+//        view.findViewById<TextView>(R.id.tvParcelName).text = "Name: ${parcel.title}"
+//        view.findViewById<TextView>(R.id.tvParcelDescription).text = "Description: ${parcel.additionalInformation}"
+//        view.findViewById<TextView>(R.id.tvParcelStatus).text = "Status: ${parcel.parcelStatus}"
+//        view.findViewById<TextView>(R.id.tvParcelSender).text = "Sender: ${parcel.sender}"
+//        view.findViewById<TextView>(R.id.tvParcelCourier).text = "Courier: ${parcel.courier}"
+//        view.findViewById<TextView>(R.id.tvParcelTrackingUrl).text = "Last Updated: ${parcel.trackingUrl}"
     }
 
     companion object {
         @JvmStatic
         fun newInstance(parcel: Parcel) =
-            ParcelDetailFragment().apply {
+            ParcelsDetailView().apply {
                 arguments = Bundle().apply {
                     putParcelable("parcel", parcel)
                 }
--- app/src/main/java/com/km/parcelorganizer/ui/parcels/ParcelsFragment.kt
+++ app/src/main/java/com/km/parcelorganizer/ui/parcels/ParcelsFragment.kt
@@ -118,8 +118,7 @@ class ParcelsFragment : BaseMVVMFragment<FragmentParcelsBinding, ParcelsViewMode
      * Hey Arthur, this is your cue to help me with kotlin classes lol
      * */
     override fun onParcelClick(parcel: Parcel) {
-//        val action = ParcelsFragmentDirections.actionParcelsFragmentToParcelsDetailView(parcel)
-        val action = ParcelsFragmentDirections.actionParcelsFragmentToParcelsDetailView2(parcel)
+        val action = ParcelsFragmentDirections.actionParcelsFragmentToParcelDetailedView(parcel)
         findNavController().navigate(action)
     }
 
